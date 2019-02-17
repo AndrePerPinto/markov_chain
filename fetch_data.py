f77ab59885ec1f12aefc437f80daa90a4c520a6d
Andre Pinto <apinto27@gmail.com>
2019-02-17 14:27:07 +0000

    Ficheiros Criados

---
+++ fetch_data.py
@@ -0,0 +1,6 @@
+'''
+Created on 17/02/2019
+
+@author: dinis
+'''
+from bs4 import BeautifulSoup
---
+++ run.py
@@ -0,0 +1,6 @@
+'''
+Created on 17/02/2019
+
+@author: dinis
+'''
+from markov_python.cc_markov import MarkovChain

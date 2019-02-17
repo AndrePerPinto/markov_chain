67d97ced3283ae68057b2ba20164367918f0a732
Andre Pinto <apinto27@gmail.com>
2019-02-17 16:50:35 +0000

    Final

--- fetch_data.py
+++ fetch_data.py
@@ -4,3 +4,31 @@ Created on 17/02/2019
 @author: dinis
 '''
 from bs4 import BeautifulSoup
+import requests
+
+html = requests.get("http://www.gutenberg.org/files/135/135-h/135-h.htm")
+
+content= html.content
+cenas=html.encoding
+
+html = html.text
+soup = BeautifulSoup(html,"html.parser")
+#print (content)
+
+
+#===============================================================================
+# with open("texto.txt","w", encoding="utf-8") as f:
+#     f.write(html)
+#===============================================================================
+with open("data.txt","w", encoding="utf-8") as fd:
+    fd.write(soup.get_text())
+print (soup.encoding)
+
+sad = 0
+list_of_words = html.split(" ")
+#print (list_of_words)
+for word in list_of_words:
+    if word == "sad":
+        sad +=1
+
+print (sad)
--- run.py
+++ run.py
@@ -4,3 +4,9 @@ Created on 17/02/2019
 @author: dinis
 '''
 from markov_python.cc_markov import MarkovChain
+
+mc = MarkovChain()
+mc.add_file("texto.txt")
+words_list = mc.generate_text()
+words_list = " ".join(words_list)
+print (words_list)
